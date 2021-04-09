3cab754c3dc8799c8a4960865c72426dedd0fed7
smtsuchi <shoha.tsuchida@gmail.com>
2021-04-09 08:36:10 -0400

    models page from database

---
+++ app/models.py
@@ -0,0 +1,31 @@
+from app import db
+
+class Divvy(db.Model):
+    trip_id = db.Column(db.Integer, primary_key = True)
+    starttime = db.Column(db.String(150), nullable = False)
+    stoptime = db.Column(db.String(150), nullable = False)
+    bikeid = db.Column(db.Integer, nullable = False)
+    from_station_id = db.Column(db.Integer, nullable = False)
+    from_station_name = db.Column(db.String, nullable = False)
+    to_station_id = db.Column(db.Integer, nullable = False)
+    to_station_name = db.Column(db.String, nullable = False)
+    usetype = db.Column(db.String, nullable = False)
+    gender = db.Column(db.String(150), nullable = True)
+    birthday = db.Column(db.String(150), nullable = True)
+    trip_duration = db.Column(db.Integer, nullable = False)
+
+    def __init__ (self, name, number, email, address, user_id):
+        self.trip_id = trip_id
+        self.starttime = starttime 
+        self.stoptime = stoptime
+        self.bikeid = bikeid
+        self.from_station_id = from_station_id
+        self.from_station_name = from_station_name
+        self.to_station_id = to_station_id 
+        self.to_station_name = to_station_name
+        self.usetype = usetype
+        self.gender = gender
+        self.birthday = birthday
+        self.trip_duration = trip_duration
+    def __repr__(self):
+        return f'<Divvy: {self.trip_id}>' #shows info in the post.query.all()
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/')
 @app.route('/index')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/calculate')
+def calculate():
+    req = request.args.to_dict()
+    start = req['start']
+    end = req['end']
+
+    entry = Divvy.query.get_or_404(entry_id)
+
+    print(req)
+    return {"A": "TEST", "B": req}
